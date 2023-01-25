a9bf794a6bfbd8766fdd3fbf4f5a367d213d2a56
dhiraj19999 <yogeshsamse682@gmail.com>
2023-01-25 19:15:10 +0530

    add

---
+++ constructor&member_function.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+class sample{
+    private:
+    int x,y;
+    public:
+    sample(){
+        
+        cout<<"hello this is a constructor";
+        
+    };
+    
+    void message(){
+       cout<<"This is member function";
+    };
+};
+
+int main(){
+    
+    sample s;
+    s.message();
+}
