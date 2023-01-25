81eb7ed60c7787681efeb21a282e146e0327aa41
dhiraj19999 <yogeshsamse682@gmail.com>
2023-01-25 19:00:44 +0530

    add

---
+++ constructor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+class constructor{
+    
+    public:
+    constructor(){
+        
+        cout<<"hello Dhiraj";
+    }
+};
+
+main(){
+    
+    constructor c;
+}
