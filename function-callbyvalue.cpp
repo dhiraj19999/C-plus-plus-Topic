d4c308b58525e2167d616185ed7f7f60c9f10e8b
dhiraj19999 <yogeshsamse682@gmail.com>
2023-03-25 10:40:18 +0530

    Add

---
+++ function-callbyvalue.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int sum(int a,int b,int c){
+
+    return a+b+c
+}
+
+
+main(){
+
+    int x,y,z;
+    cout<<"Enter three numbers";
+    cin>>x>>y>>z;
+    cout<<"sum of three numbers is"<<sum(x,y,z)
+
+}
