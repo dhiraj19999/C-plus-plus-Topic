49ea59c7a6b06d4e66f92a5f9199cf8426025838
dhiraj19999 <yogeshsamse682@gmail.com>
2023-03-29 09:23:06 +0530

    add

---
+++ static_members_class.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+class demo{
+
+static int a;
+public:
+static void display(){
+    count<<"value of a is"<<a
+}
+
+}
+
+int demo::a::10;
+
+main(){
+
+    demo obj
+    obj.display()
+}
