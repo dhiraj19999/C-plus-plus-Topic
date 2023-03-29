62c18f6140b4c87e2e8c19cf1d73e7fff17fe9a8
dhiraj19999 <yogeshsamse682@gmail.com>
2023-03-29 08:52:02 +0530

    add

---
+++ objectasan_argument.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+class sample {
+
+    int x=10;
+
+    void display(sample s1){
+
+        cout<<"value of x accsed by passing object is"<<s.x
+    }
+}
+
+main(){
+
+    sample s1
+    s1.display(s1)
+}
