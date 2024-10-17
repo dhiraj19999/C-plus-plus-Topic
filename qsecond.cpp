ac7c31333c0b6b697b047d5d66efc309079e59a8
Dhiraj Garad <dhirajgarad46@gmail.com>
2024-10-17 22:51:11 +0530

    Update qsecond.cpp

--- qsecond.cpp
+++ qsecond.cpp
@@ -105,12 +105,14 @@ int main()
     return 0;
 }
 
-/*
+
 
 //A constructor is a specially defined method in a C++ class that is automatically called when an instance of that class is created,constructor has same name as Class name
+// Default Constructor: No parameters. They are used to create an object with default values.
+//Parameterized Constructor: Takes parameters. Used to create an object with specific initial values.
+//Copy Constructor: Takes a reference to another object of the same class. Used to create a copy of an object.
 
 
-*\
 
 
 #include <iostream>
@@ -156,7 +158,7 @@ class student
     private:
         int x, y ;
     public:
-        student(int t1, int t2) // Parameterized constructor
+        student(int t1, int t2) // Parameterized or argument constructor
         {
             x = t1 ; y = t2 ; 
         }
@@ -234,7 +236,7 @@ class student
     private:
         int x, y ;
     public:
-        student(int t1, int t2=0)   // Constructors
+        student(int t1, int t2=0)   //  arguments constructor
         {
             x = t1 ; y = t2 ; 
         }
@@ -258,4 +260,185 @@ int main()
     s3.putdata();
     
     return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int sum(int *t1, int *t2, int *t3)
+{
+    int total ;
+    cout << "The address of x (stored t1 ) : " << t1 << endl ;
+    cout << "The address of y (stored t2 ) : " << t2 << endl ;
+    cout << "The address of z (stored t3 ) : " << t3 << endl ;
+    
+    total = *t1 + *t2 + *t3 ; 
+    return(total);
+}
+
+int main()
+{
+    int x = 10, y = 20, z = 30 ; 
+    int result ; 
+     result = sum(x, y, z); // Call by value that is passes the value 
+    result = sum(&x, &y, &z);   //Call by reference that is passes the address of value
+    cout << "The result of sum = " << result << endl ;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+class Student
+{
+    private: 
+    int x, y ; 
+    public:
+    Student(int t1, int t2)    // Argument Constructor
+    {
+        x = t1 ; y = t2 ; 
+    }
+    
+    Student(Student &t)      //Copy Constructor
+    {
+        x = t.x ; 
+        y = t.y ; 
+    }
+    
+    void putdata()
+    {
+        cout << "The Value of x = " << x << endl ;
+        cout << "The value of y = " << y << endl ; 
+    }
+};
+
+int main()
+{
+    Student s1(100,200), s2(400, 200), s3(100,400), s4(s2) ; 
+    cout << "Object s1 " <<endl;
+    s1.putdata();
+    cout << "Object s2 " <<endl;
+    s2.putdata();
+    cout << "Object s3 " <<endl;
+    s3.putdata();
+    cout << "Object s4 " <<endl;
+    s4.putdata();
+    return 0;
+}
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+//Write a program to select maximum value from given three values.
+// 10, 34, 25
+// Output should be : Maximum value is 34.
+
+class NumberCheck
+{
+    int x, y, z ; 
+    public:
+    NumberCheck(int t1, int t2, int t3)
+    {
+        x = t1 ; y = t2 ; z = t3 ; 
+    }
+    void SelectMax()
+    {
+         if (x > y ) 
+         {
+             if (x > z ) {   cout << "x is big " ; }
+             else {  cout << "z is big" ;  }
+         }
+         else 
+         { 
+             if (y > z ) { cout << "y is big"; }
+             else
+             { cout <<"z is big"; }
+             
+         }
+    }
+};
+
+int main()
+{
+    NumberCheck s1(500, 134, 87); 
+    s1.SelectMax();
+    return 0;
+}
+
+
+
+
+#include <iostream>
+using namespace std;
+
+//Write a program to add the sequence of numbers from 10 to 25.
+// Output should be : Total value is  = **** .
+
+int main()
+{
+    cout << "Welcome to the class " << endl ;
+    int sum = 0 ;
+    
+    for (int i = 10; i <= 25 ; i=i+1) // i++
+    {
+        sum = sum + i ; 
+    }
+    cout << "Total value is = " << sum << endl ;
+    return 0 ;
+
+
+
+
+
+
+}
+
+
+
+
+#include <iostream>
+using namespace std;
+
+// Write a program to display EVEN NUMBERS from 10 to 25.
+// Display only the summation of even numbers
+
+int main()
+{
+    int sum = 0 ;
+    for (int i = 10; i <= 25 ; i=i+1) // i++
+    {
+        if ( i % 2 == 0 )
+        {
+            cout << i << "is even number " << endl ;
+            sum = sum + i ; 
+        }
+    }
+    cout << "The total value is = " << sum << endl ;
+    return 0 ;
 }
