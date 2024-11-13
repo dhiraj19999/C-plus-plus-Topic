ce1347cb5d6ee332476548111c875509b9d4adc8
Dhiraj Garad <yogeshsamse682@gmail.com>
2024-11-13 21:21:36 +0530

    Update Nine.cpp

--- Nine.cpp
+++ Nine.cpp
@@ -16,3 +16,77 @@ int main(void) {
      cout<<"Bonus: "<<p1.bonus<<endl;    
     return 0;  
 }  
+
+
+
+Virtual base classes are used in virtual inheritance in a way of preventing multiple “instances” of a given class appearing in an inheritance hierarchy when using multiple inheritances. 
+
+Need for Virtual Base Classes: Consider the situation where we have one class A . This class A is inherited by two other classes B and C. Both these class are inherited into another in a new class D 
+
+#include <iostream> 
+using namespace std; 
+
+class A { 
+public: 
+    void show() 
+    { 
+        cout << "Hello from A \n"; 
+    } 
+}; 
+
+class B : public A { 
+}; 
+
+class C : public A { 
+}; 
+
+class D : public B, public C { 
+}; 
+
+int main() 
+{ 
+    D object; 
+    object.show(); 
+} 
+Compile Errors:
+
+prog.cpp: In function 'int main()':
+prog.cpp:29:9: error: request for member 'show' is ambiguous
+  object.show();
+         ^
+prog.cpp:8:8: note: candidates are: void A::show()
+   void show()
+        ^
+prog.cpp:8:8: note:                 void A::show()
+
+
+To resolve this ambiguity when class A is inherited in both class B and class C, it is declared as virtual base class by placing a keyword virtual as : 
+
+virtual can be written before or after the public. Now only one copy of data/function member will be copied to class C and class B and class A becomes the virtual base class. Virtual base classes offer a way to save space and avoid ambiguities in class hierarchies that use multiple inheritances. When a base class is specified as a virtual base, it can act as an indirect base more than once without duplication of its data members. A single copy of its data members is shared by all the base classes that use virtual base. 
+
+
+#include <iostream> 
+using namespace std; 
+
+class A { 
+public: 
+    void show() 
+    { 
+        cout << "Hello from A \n"; 
+    } 
+}; 
+
+class B : public virtual A { 
+}; 
+
+class C : public virtual A { 
+}; 
+
+class D : public B, public C { 
+}; 
+
+int main() 
+{ 
+    D object; 
+    object.show(); 
+} 
