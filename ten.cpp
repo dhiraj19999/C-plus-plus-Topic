ca9b06cc4860f34f3b904181f57db5ffa67527a5
Dhiraj Garad <yogeshsamse682@gmail.com>
2024-11-15 20:13:47 +0530

    Update ten.cpp

--- ten.cpp
+++ ten.cpp
@@ -125,3 +125,49 @@ return 0;
 }
 
 
+
+
+
+A virtual function (also known as virtual methods) is a member function that is declared within a base class and is re-defined (overridden) by a derived class. When you refer to a derived class object using a pointer or a reference to the base class, you can call a virtual function for that object and execute the derived class’s version of the method.
+
+
+
+// C++ program to illustrate
+// concept of Virtual Functions
+
+#include <iostream>
+using namespace std;
+
+class base {
+public:
+    virtual void print() { cout << "print base class\n"; }
+
+    void show() { cout << "show base class\n"; }
+};
+
+class derived : public base {
+public:
+    void print() { cout << "print derived class\n"; }
+
+    void show() { cout << "show derived class\n"; }
+};
+
+int main()
+{
+    base* bptr;
+    derived d;
+    bptr = &d;
+
+    // Virtual function, binded at runtime
+    bptr->print();
+
+    // Non-virtual function, binded at compile time
+    bptr->show();
+
+    return 0;
+}
+
+
+
+print derived class
+show base class
