b5552c86aff91bcac5e3f028b32c2b2e492974c5
Dhiraj Garad <yogeshsamse682@gmail.com>
2024-11-15 20:06:52 +0530

    Update

---
+++ ten.cpp
@@ -0,0 +1,127 @@
+What is Function Overriding in C++?
+Function overriding is a type of polymorphism in which we redefine the member function of a class which it inherited from its base class. The function signature remains same but the working of the function is altered to meet the needs of the derived class. So, when we call the function using its name for the parent object, the parent class function is executed. But when we call the function using the child object, the child class version will be executed.
+
+
+// C++ Program to illustrate how to implement runtime
+// function overriding using virtual function
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    // Declare the function as virtual to allow overriding
+    // in derived classes
+    virtual void display()
+    {
+        cout << "Display method of Base class" << endl;
+    }
+
+    // Virtual destructor to ensure proper cleanup of
+    // derived class objects
+    virtual ~Base() {}
+};
+
+class Derived : public Base {
+public:
+    // Override the display method
+    void display() override
+    {
+        cout << "Display method of Derived class" << endl;
+    }
+};
+
+int main()
+{
+    Base* basePtr;
+    Derived derivedObj;
+
+    // Point base class pointer to derived class object
+    basePtr = &derivedObj;
+
+    // Call the display function
+    // This will call the display method of the Derived
+    // class due to the virtual function mechanism
+    basePtr->display();
+
+    return 0;
+}
+
+
+output= Display method of Derived class
+
+
+
+
+Sometimes implementation of all functions cannot be provided in a base class because we don’t know the implementation. Such a class is called an abstract class.For example, let Shape be a base class. We cannot provide the implementation of function draw() in Shape, but we know every derived class must have an implementation of draw(). Similarly, an Animal class doesn’t have the implementation of move() (assuming that all animals move), but all animals must know how to move. We cannot create objects of abstract classes.
+
+A pure virtual function (or abstract function) in C++ is a virtual function for which we can have an implementation, But we must override that function in the derived class, otherwise, the derived class will also become an abstract class. A pure virtual function is declared by assigning 0 in the declaration
+
+
+
+// C++ program to illustrate the abstract class with pure
+// virtual functions
+#include <iostream>
+using namespace std;
+
+class Test {
+    // private member variable
+    int x;
+
+public:
+    // pure virtual function
+    virtual void show() = 0;
+
+    // getter function to access x
+    int getX() { return x; }
+};
+
+int main(void)
+{
+    // Error: Cannot instantiate an abstract class
+    Test t; // we can't make object of abstract class
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+class A    // Abstract Class
+{
+public:
+void showMyProfile()
+{
+cout << "LOVELY PROFESSIONAL UNIVERSITY" << endl ; 
+}
+virtual void showA() = 0 ;  // Pure Virtual Function 
+};
+
+class B : public A
+{
+public:
+ void showA() { cout << "This is showA in class B \n"; }
+};
+
+/*
+class C : public A 
+{
+public:
+ void showA() { cout << "This is showA in class B \n"; }
+};
+*/
+
+int main()
+{
+B obj ; 
+obj.showMyProfile() ; 
+obj.showA();
+
+return 0;
+}
+
+
