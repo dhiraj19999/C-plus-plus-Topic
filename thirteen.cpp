3f69aca521bcc3cbb5b7a93f22f801f918447ea0
Dhiraj Garad <yogeshsamse682@gmail.com>
2024-11-26 23:32:35 +0530

    Update thirteen.cpp

--- thirteen.cpp
+++ thirteen.cpp
@@ -84,3 +84,22 @@ catch(int temp)  //default catch block
 }
 cout << "Program is Completed \n";
 }
+
+
+int main()  
+{
+int x ; 
+
+try
+{
+throw(x) ;  //Termination Model
+cout << "Statement inside Try block \n";
+}
+
+catch(...)  //default catch block
+{
+cout << "Kindly try again,, sorry for inconvenience\n";
+}
+
+cout << "Program is Completed \n";
+}
