aaa6ed6aa2ea02682744c00b5a5b51e65c5a4ffe
Dhiraj Garad <yogeshsamse682@gmail.com>
2024-11-26 23:08:41 +0530

    Update thirteen.cpp

--- thirteen.cpp
+++ thirteen.cpp
@@ -18,3 +18,39 @@ cout << "Kindly try again,, sorry for inconvenience\n";
 
 cout << "Program is Completed \n";
 }
+
+
+
+
+int main()  
+{
+int x = 100; 
+float y = 12.45; 
+char z = 'A'; 
+double x1 = 3467899.123; 
+
+try
+{
+throw(x) ;  
+cout << "Statement inside Try block \n";
+}
+catch(int)  //default catch block
+{
+cout << " int handling" << endl;
+}
+catch(float)  //default catch block
+{
+cout << "float handling " << endl;
+}
+catch(char)  //default catch block
+{
+cout << "char handling " << endl;
+}
+catch(double)  //default catch block
+{
+cout << " double handling" << endl;
+}
+
+
+cout << "Program is Completed \n";
+}
